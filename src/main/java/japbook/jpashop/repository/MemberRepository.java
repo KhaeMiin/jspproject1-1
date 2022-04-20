f4865ba6a568c52f2ff029a148288fb7aea39943
minMin <goals1005@naver.com>
2022-04-20 14:08:22 +0900

    회원01. 회원 리포지토리 개발

---
+++ src/main/java/japbook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,35 @@
+package japbook.jpashop.repository;
+
+import japbook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) { //단건조회
+        return em.find(Member.class, id); //find(type, pk)
+    }
+
+    public List<Member> findAll() { //회원 모두 조회
+        return em.createQuery("select m from Member m", Member.class)
+                //jpql(qlString): sql과 기능적으로는 거의 동일하지만 from의 대상이 테이블을 대상으로 쿼리를 하지만 얘는 Entity객체를 대상으로 함
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) { //회원 이름으로 조회
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
