57d5d67f775b970fc3ea95ced963bc217dec017c
minMin <goals1005@naver.com>
2022-04-21 22:13:55 +0900

    상품 도메인 개발

--- src/main/java/japbook/jpashop/domain/item/Item.java
+++ src/main/java/japbook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package japbook.jpashop.domain.item;
 
 import japbook.jpashop.domain.Category;
+import japbook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,7 +12,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
-@Getter @Setter
+@Getter
 public abstract class Item {
 
     @Id
@@ -25,5 +26,25 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==// (데이터를 가지고 있는 클래스에 핵심 비즈니스 로직이 있는 것이 좋다)
+
+    /**
+     * stock(재고) 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock(재고) 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 
 }
